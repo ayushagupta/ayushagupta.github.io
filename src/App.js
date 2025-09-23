314a98e24f123409dcb61df2d02d4dfef66d1aa4
Ayush <mumbai.ayushgupta@gmail.com>
2025-09-23 14:37:17 -0400

    feat(routing): switch to BrowserRouter and add GH Pages SPA fallback (clean URLs)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Header from './components/Header';
